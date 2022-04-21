0edadf29d6fafb5047111506be4b1d9992f5911d
jardy129 <32240587+jardy129@users.noreply.github.com>
2022-04-21 19:33:13 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,6 +6,5 @@
 #=================================================
 #1. 修改默认的IP以
 sed -i 's/192.168.1.1/192.168.2.5/g' openwrt/package/base-files/files/bin/config_generate
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
 
