2a8fd8e8fcd729702cdc4f96dedc2a935f4bb053
jardy129 <32240587+jardy129@users.noreply.github.com>
2022-05-01 08:58:25 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,7 +5,7 @@
 # Author: Jardy
 #=================================================
 #1. 修改默认的IP以
-sed -i 's/192.168.1.1/192.168.2.3/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.5/g' openwrt/package/base-files/files/bin/config_generate
 #2. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
 
